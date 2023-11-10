dcab9f63393484e2e841d59595e8612a7b933967
metaIyst <149410140+metaIyst@users.noreply.github.com>
2023-11-10 15:13:14 +0300

    Commit 5.1

--- src/LabWork18/Task2.java
+++ src/LabWork18/Task2.java
@@ -6,12 +6,14 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        String intString = scanner.next();
-        int i = Integer.parseInt(intString);
         try {
+            String intString = scanner.next();
+            int i = Integer.parseInt(intString);
             System.out.println(7 / i);
         } catch (Exception ex) {
             System.out.println("ArithmeticException was thrown");
+        } finally {
+            scanner.close();
         }
     }
 }
