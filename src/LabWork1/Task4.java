8c84be111fa936400001b818568f3e24052d9e5f
metaIyst <149410140+metaIyst@users.noreply.github.com>
2023-11-10 15:27:26 +0300

    Commit 5.3

--- src/LabWork1/Task4.java
+++ src/LabWork1/Task4.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         int[] arr = new int[5];
 
-        try {
+        try (Scanner scanner = new Scanner(System.in)) {
             for (int i = 0; i < arr.length; i++) {
                 System.out.println("Enter number: ");
                 arr[i] = scanner.nextInt();
             }
-        } finally {
-            scanner.close();
         }
         
         int arrLen = arr.length - 1;
